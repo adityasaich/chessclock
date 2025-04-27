7c0f9aff9b37571b9431e1fe51ef4a7552de1c5b
Aditya Sai Chappidi <achappidi@salesforce.com>
2025-04-27 19:02:06 +0530

    added using only refs and no effects

--- chess-clock/src/GameScreen.js
+++ chess-clock/src/GameScreen.js
@@ -8,56 +8,57 @@ function  GameScreen({time,increment}) {
   const [player1Time, setPlayer1Time] = useState(time * 60);
   const [player2Time, setPlayer2Time] = useState(time * 60);
   const [gameStarted, setGameStarted] = useState(false);
-  const [gameEnded, setGameEnded] = useState(false);
+  
+  const player1ActiveRef = useRef(isPlayer1Active);
+  const player1TimeRef = useRef(player1Time);
+  const player2TimeRef = useRef(player2Time);
+  const intervalTimeRef = useRef(null);
+  
+  const setPlayer1Active = (active) => {
+    player1ActiveRef.current = active;
+    setIsPlayer1Active(active);
+  }
 
-  const prevIsPlayer1Active = useRef(true);
+  const setPlayerTime1 = (time) => {
+    player1TimeRef.current = time;
+    setPlayer1Time(time);
+  }
+
+  const setPlayerTime2 = (time) => {
+    player2TimeRef.current = time;
+    setPlayer2Time(time);
+  }
+
+  const timer = () => {
+    if(player1ActiveRef.current) {
+        setPlayerTime1(player1TimeRef.current - 1);
+    } else {
+        setPlayerTime2(player2TimeRef.current - 1);
+    }
+  }
+
+  let startGame = () =>  {
+      intervalTimeRef.current = setInterval(timer, 1000);
+  };
+
+  const handlePlayerMove = () => {
+    if(!gameStarted) {
+        setPlayer1Active(isPlayer1White);
+        setGameStarted(true);
+        startGame();
+    } else {
+        setPlayer1Active(!isPlayer1Active)
+    }
+  }
+
 
   const swapPlayers = () => {
     setIsPlayer1White((p1White) => !p1White);
   }
 
-  useEffect(() => {
-    if(player2Time === 0 || player1Time === 0){
-        setGameEnded(true);
-    }
-  }, [player2Time, player1Time])
-
-  useEffect(() => {
-    
-    const decrementTime = () => {
-        if(isPlayer1Active) {
-            setPlayer1Time(pt => pt - 1);
-        } else {
-            setPlayer2Time(pt => pt - 1);
-        }
-      }
-       
-      if(gameStarted && !gameEnded){
-        if(prevIsPlayer1Active.current &&!isPlayer1Active) {
-          prevIsPlayer1Active.current = false;
-          setPlayer1Time(pt => pt + increment);     
-        } else if (!prevIsPlayer1Active.current && isPlayer1Active){
-          prevIsPlayer1Active.current = true;
-          setPlayer2Time(pt => pt + increment);
-        }
-        const myInterval = setInterval(decrementTime, 1000);
-        return () => clearInterval(myInterval);
-      }
-  }, [isPlayer1Active, gameStarted, gameEnded, increment]);
-
-  const playGame = () => {
-    if(!gameStarted) {
-        setIsPlayer1Active(isPlayer1White);
-        prevIsPlayer1Active.current = isPlayer1Active;
-        setGameStarted(true);
-    } else {
-        setIsPlayer1Active(pa => !pa)
-    }
-  }
-
   return (
     <div style={{height: "100vh",textAlign: "center",display: "flex",flexDirection: "column"}}>
-    <Player onClick={playGame} time={player1Time} isWhiteColor={isPlayer1White} />
+    <Player onClick={handlePlayerMove} time={player1Time} isWhiteColor={isPlayer1White} />
     { !gameStarted &&
       <div
         onClick={swapPlayers}
@@ -71,7 +72,7 @@ function  GameScreen({time,increment}) {
           height: "100%"
         }}> Swap White And Black</h1>
       </div> }
-    <Player onClick={playGame} time={player2Time} isWhiteColor={!isPlayer1White} />
+    <Player onClick={handlePlayerMove} time={player2Time} isWhiteColor={!isPlayer1White} />
     </div>
   );
 }
