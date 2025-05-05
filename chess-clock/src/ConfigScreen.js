011df01ea12392e1920ef094ce7caa090d46a3c8
Aditya Sai Chappidi <achappidi@salesforce.com>
2025-05-05 21:50:10 +0530

    adding default values

--- chess-clock/src/ConfigScreen.js
+++ chess-clock/src/ConfigScreen.js
@@ -3,9 +3,9 @@ import "./App.css";
 function ConfigScreen({onStart}) {
 
   const startGame = () => {
-    let timeInMinutes = document.getElementById('timerInput').value;
+    let timeInMinutes = document.getElementById('timerInput').value || 10;
     timeInMinutes = parseInt(timeInMinutes, 10)
-    let incTime = document.getElementById('incTime').value;
+    let incTime = document.getElementById('incTime').value || 5;
     incTime = parseInt(incTime, 10)
     onStart(timeInMinutes, incTime);
   }
@@ -14,7 +14,7 @@ function ConfigScreen({onStart}) {
     <div style={{"text-align":"center"}}>
       <div>
         <label style={{"font-size": "24px"}} for="timerInput">Enter Player Time In Minutes </label>
-        <input style={{"height": "24px"}}  type="number" list="timers" id="timerInput" />
+        <input style={{"height": "24px"}}  type="number" list="timers" id="timerInput" placeholder="10"/>
         <datalist id="timers">
           <option>1</option>
           <option>3</option>
@@ -25,7 +25,7 @@ function ConfigScreen({onStart}) {
       <br></br>
       <div>
         <label style={{"font-size": "24px"}} for="incTime" >Enter Move Increment In Seconds </label>
-        <input style={{"height": "24px"}}  type="number" list="increments" id="incTime" />
+        <input style={{"height": "24px"}}  type="number" list="increments" id="incTime" placeholder="5"/>
         <datalist id="increments">
           <option>0</option>
           <option>2</option>
